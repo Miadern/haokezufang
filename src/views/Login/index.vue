2ac7c1a5914ff88dff302d20f20a3ac0e1b3da07
Miadern <465029437@qq.com>
2022-07-13 18:20:36 +0800

    初始化项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <!-- 头部导航 -->
+    <van-nav-bar title="标题" left-arrow @click-left="onClickLeft" />
+    <!-- 表单 -->
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClickLeft() {}
+  }
+}
+</script>
+
+<style></style>
